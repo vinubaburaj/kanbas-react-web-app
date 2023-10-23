4854dbf70259f3ed4a11c8be4fd18d5a636b8c27
Vinu Baburaj <vinubaburaj@gmail.com>
2023-10-22 23:52:46 -0400

    Implemented Kanbas

--- src/Labs/a3/index.js
+++ src/Labs/a3/index.js
@@ -5,11 +5,21 @@ import Classes from "./Classes";
 import Styles from "./Styles";
 import ConditionalOutput from "./ConditionalOutput";
 import TodoList from "./todo/TodoList";
+import { useSelector } from "react-redux";
 
 function Assignment3() {
+  const { todos } = useSelector((state) => state.todosReducer);
+
   return (
     <div className="container">
       <h1>Assignment 3</h1>
+      <ul className="list-group">
+        {todos.map((todo) => (
+          <li className="list-group-item" key={todo.id}>
+            {todo.title}
+          </li>
+        ))}
+      </ul>
       <TodoList />
       <ConditionalOutput />
       <Styles />
