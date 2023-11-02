62d76b445428a2e2fc91a1fa0df2dd99300161dc
Vinu Baburaj <vinubaburaj@gmail.com>
2023-11-01 23:39:49 -0400

    Implemented delete assignment

--- src/Kanbas/Courses/Assignments/index.js
+++ src/Kanbas/Courses/Assignments/index.js
@@ -3,14 +3,21 @@ import { Link, useParams } from "react-router-dom";
 import db from "../../Database";
 import { AiOutlinePlus, AiFillCheckCircle } from "react-icons/ai";
 import { HiOutlineEllipsisVertical } from "react-icons/hi2";
-import { TfiPencilAlt } from 'react-icons/tfi'; 
+import { TfiPencilAlt } from "react-icons/tfi";
 import "./index.css";
 import { useSelector, useDispatch } from "react-redux";
-import { addAssignment, updateAssignment, deleteAssignment, selectAssignment } from "./assignmentsReducer";
+import {
+  addAssignment,
+  updateAssignment,
+  deleteAssignment,
+  selectAssignment,
+} from "./assignmentsReducer";
 
 function Assignments() {
   const { courseId } = useParams();
-  const assignments = useSelector((state) => state.assignmentsReducer.assignments);
+  const assignments = useSelector(
+    (state) => state.assignmentsReducer.assignments
+  );
   const courseAssignments = assignments.filter(
     (assignment) => assignment.course === courseId
   );
@@ -30,10 +37,10 @@ function Assignments() {
             Group
           </button>
           <Link to={`/Kanbas/Courses/${courseId}/Assignments/addAssignment`}>
-          <button class="btn btn-danger">
-            <AiOutlinePlus />
-            Assignment
-          </button>
+            <button class="btn btn-danger">
+              <AiOutlinePlus />
+              Assignment
+            </button>
           </Link>
           <button class="btn wd-bg-lightgray">
             <HiOutlineEllipsisVertical />
@@ -65,6 +72,17 @@ function Assignments() {
             <TfiPencilAlt color="green" />
             <span className="wd-assignment-title ms-2">{assignment.title}</span>
             <span class="wd-check-ellipse-button-float-end float-end">
+              <button
+                class="btn btn-danger me-1"
+                onClick={(e) => {
+                  e.preventDefault();
+                  dispatch(deleteAssignment(assignment._id))
+                }}
+              >
+                Delete
+              </button>
+            
+
               <AiFillCheckCircle color="green" />
               <HiOutlineEllipsisVertical />
             </span>
