d8782a8fca09d637d4cfbb019430d53893e3f992
Vinu Baburaj <vinubaburaj@gmail.com>
2023-11-05 18:26:09 -0500

    Implemented working with objects

---
+++ src/Labs/a5/index.js
@@ -0,0 +1,20 @@
+import EncodingParametersInURLs from "./EncodingParametersInURLs";
+import WorkingWithObjects from "./WorkingWithObjects";
+import SimpleAPIExamples from "./simpleapiexamples";
+
+function Assignment5() {
+  return (
+    <div>
+      <h1>Assignment 5</h1>
+      <div className="list-group">
+        <a href="http://localhost:4000/a5/welcome" className="list-group-item">
+          Welcome
+        </a>
+      </div>
+      <SimpleAPIExamples />
+      <EncodingParametersInURLs />
+      <WorkingWithObjects />
+    </div>
+  );
+}
+export default Assignment5;
---
+++ src/Labs/a5/simpleapiexamples.js
@@ -0,0 +1,8 @@
+const SimpleAPIExamples = () => {
+    return(
+        <>
+        <h3>Simple API Examples</h3>
+        </>
+    )
+}
+export default SimpleAPIExamples;
