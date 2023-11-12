c68e632d881977093434905e365adcd7763fa015
Vinu Baburaj <vinubaburaj@gmail.com>
2023-11-12 12:13:18 -0500

    Implemented Modules

--- src/Kanbas/Courses/Modules/ModuleList.js
+++ src/Kanbas/Courses/Modules/ModuleList.js
@@ -1,20 +1,47 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import "./index.css";
-import { LuGripVertical } from 'react-icons/lu'
+import { LuGripVertical } from "react-icons/lu";
 import { useSelector, useDispatch } from "react-redux";
+import * as service from "./service";
+import { useEffect } from "react";
 import {
   addModule,
   updateModule,
   deleteModule,
   setModule,
+  setModules,
 } from "./modulesReducer";
 
 function ModuleList() {
   const { courseId } = useParams();
+
+  useEffect(() => {
+    service.findModulesForCourses(courseId).then((modules) =>
+      dispatch(setModules(modules))
+    );
+  }, [courseId]);
+
   const modules = useSelector((state) => state.modulesReducer.modules);
   const module = useSelector((state) => state.modulesReducer.module);
   const dispatch = useDispatch();
+
+  const handleAddModule = () => {
+    service.createModuleForCourse(courseId, module).then((module) => {
+      dispatch(addModule(module));
+    });
+  };
+
+  const handleDeleteModule = (moduleId) => {
+    service.deleteModule(moduleId).then((status) => {
+      dispatch(deleteModule(moduleId));
+    });
+  };
+
+  const handleUpdateModule = async (module) => {
+    const status = await service.updateModule(module);
+    dispatch(updateModule(module));
+  }
 
   return (
     <ul className="list-group wd-modules-list">
@@ -45,17 +72,12 @@ function ModuleList() {
           }
         />
         <br />
-        <button
-          className="btn btn-success"
-          onClick={() =>
-            dispatch(addModule({ ...module, course: courseId }))
-          }
-        >
+        <button className="btn btn-success" onClick={handleAddModule}>
           Add Module
         </button>
         <button
           className="btn btn-primary ms-2"
-          onClick={() => dispatch(updateModule(module))}
+          onClick={() => handleUpdateModule(module)}
         >
           Update Module
         </button>
@@ -74,28 +96,24 @@ function ModuleList() {
               </button>
               <button
                 className="btn btn-danger ms-1"
-                onClick={() => dispatch(deleteModule(module._id))}
+                onClick={() => handleDeleteModule(module._id)}
               >
                 Delete
               </button>
             </div>
             <h5>{module.name}</h5>
             <p>{module.description}</p>
-            {
-                module.lessons && (
-                    <ul className="list-group">
-                        {
-                            module.lessons.map((lesson, index) => (
-                                <li key={index} className="list-group-item">
-                                  <LuGripVertical />
-                                    {lesson.name}
-                                    {/* <p>{lesson.description}</p> */}
-                                </li>
-                            ))
-                        }
-                    </ul>
-                )
-             }
+            {module.lessons && (
+              <ul className="list-group">
+                {module.lessons.map((lesson, index) => (
+                  <li key={index} className="list-group-item">
+                    <LuGripVertical />
+                    {lesson.name}
+                    {/* <p>{lesson.description}</p> */}
+                  </li>
+                ))}
+              </ul>
+            )}
           </li>
         ))}
     </ul>
